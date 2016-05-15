0d08ee51e391ecdff2e35ac5bfc9de820daadbec
subertbalazs <balazs.subert@gmail.com>
2016-05-15 15:27:59 +0200

    message validation works

--- js/main.js
+++ js/main.js
@@ -1,20 +1,61 @@
- function validateName()
-    {
-        var name = document.myForm.Name.value;
-        if (name.length > 30 || name.length < 3) {
-            alert("The name must be at least 3 chars and less than 30 chars!");
-            return false;
-        }
-        return true;
+function validateNameLength()
+{
+    var name = document.myForm.Name.value;
+    if (name.length > 30 || name.length < 3) {
+        alert("The name must be at least 3 chars and less than 30 chars!");
+        return false;
     }
+    return true;
+}
 
-    function validateEmail()
+function validateNameWhiteSpace() {
+    var name = document.myForm.Name.value;
+    var counter = 0;
+    for (var i = 0; i < name.length; i++)
     {
-        var email = document.myForm.Email.value.toString();
-        if (!email.endsWith(".hu") || !email.endsWith(".com"))
+        if (name[i] == " ")
         {
-            alert("The email address is not valid!")
-            return false;
+            counter ++;
         }
-        return true;
     }
+    if(counter == 0 )
+    {
+        alert("The name should contain at least one whitespace bewteen first and lastname.")
+        return false;
+    }
+    return true;
+}
+
+function validateEmail()
+{
+    var email = document.myForm.Email.value.toString();
+    if (email.search(".*\.(hu|com)$"))
+    {
+        alert("The email address is not valid!");
+        return false;
+    }
+    return true;
+}
+
+function validateMessage()
+{
+    var message = document.myForm.textarea.value;
+    if(message.length < 20 || message.length > 500)
+    {
+        alert("The message is valid in case of more than 20 and less than ")
+        return false;
+    }
+    return true;
+}
+
+function validateForm()
+{
+    var validation = true;
+    validation &= validateNameLength();
+    validation &= validateNameWhiteSpace();
+    validation &= validateEmail();
+    validation &= validateMessage();
+  
+    return validation;
+}
+
